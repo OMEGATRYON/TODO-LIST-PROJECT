85f72200d1fc5791c3f20411c560d19c37bae6fa
OMEGATRYON <tryon.baldwin@student.awesomeinc.org>
2023-03-01 21:53:27 +0000

    added list and header, on main branch, switching to dev

---
+++ app/src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./comps/Header";
+import Todolist from "./comps/Todolist";
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <Todolist />
+        </div>
+    );
+}
+export default App;
---
+++ app/src/comps/Todolist.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function Todolist() {
+    const [items, setItems] = useState([]);
+    const [inputValue, setInputValue] = useState([]);
+
+    console.log(items);
+
+    function handleInputChange(event) {
+        setInputValue(event.target.value);
+    }
+
+    function handleClick(event) {
+        setItems([...items, inputValue]);
+    }
+
+    return (
+        <div>
+            <input type="text" placeholder="whats on the list!?" onChange={handleInputChange}/>
+            <button onClick={handleClick}>Add Todo!</button>
+            <ul>
+                {items.map(items => {
+                    return <li key={items}>{items}</li>
+                })}   
+            </ul>
+        </div>
+    )
+}
+export default Todolist;
