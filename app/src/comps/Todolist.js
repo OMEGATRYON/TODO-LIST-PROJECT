2349637cd14da9996bef428fae9de6e7b37c872b
OMEGATRYON <tryon.baldwin@student.awesomeinc.org>
2023-03-01 23:19:19 +0000

    added gray line

--- app/src/comps/Todolist.js
+++ app/src/comps/Todolist.js
@@ -13,12 +13,27 @@ function Todolist() {
     function handleClick(event) {
         setItems([...items, inputValue]);
     }
+    const lineClick = event => {
+        if (event.target.style.textDecoration) {
+          event.target.style.removeProperty('text-decoration');
+        } else {
+          event.target.style.setProperty('text-decoration', 'line-through',);
+        }
+      };
+      //click turns gray but NOT line-through. HOW TO FIX!
+      const lineClickred = event => {
+        if (event.target.style.textDecoration) {
+          event.target.style.removeProperty('text-decoration');
+        } else {
+          event.target.style.setProperty('color', 'gray');
+        }
+      };
 
     return (
         <div>
             <input type="text" placeholder="whats on the list!?" onChange={handleInputChange}/>
             <button onClick={handleClick}>Add to list!</button>
-            <ul>
+            <ul onClick={lineClick} onClick={lineClickred}>
                 {items.map(items => {
                     return <li key={items}>{items}</li>
                 })}   
